e40c46c151f32d40a95053dd20e4649d36d2cb14
Michał Moskal <michal.moskal@10clouds.com>
2017-08-28 11:41:53 +0200

    Update csv files location

--- titan/titan/settings_dev.py
+++ titan/titan/settings_dev.py
@@ -131,14 +131,15 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATIC_URL = '/static/'
 
-PATH_TO_CSV_CHART = os.path.join(BASE_DIR, 'titan-chart.csv')
-PATH_TO_CSV_CHART_YTD = os.path.join(BASE_DIR, 'titan-chart-ytd.csv')
-PATH_TO_CSV_CHART_1Y = os.path.join(BASE_DIR, 'titan-chart-1y.csv')
-PATH_TO_CSV_CHART_3Y = os.path.join(BASE_DIR, 'titan-chart-3y.csv')
-PATH_TO_CSV_CHART_5Y = os.path.join(BASE_DIR, 'titan-chart-5y.csv')
-PATH_TO_CSV_CHART_10Y = os.path.join(BASE_DIR, 'titan-chart-10y.csv')
-PATH_TO_CSV_CHART_ALL = os.path.join(BASE_DIR, 'titan-chart-all.csv')
-PATH_TO_CSV_STATS = os.path.join(BASE_DIR, 'titan-stats.csv')
+CSV_DIR = os.path.join(BASE_DIR, 'charts')
+PATH_TO_CSV_CHART = os.path.join(CSV_DIR, 'titan-chart.csv')
+PATH_TO_CSV_CHART_YTD = os.path.join(CSV_DIR, 'titan-chart-ytd.csv')
+PATH_TO_CSV_CHART_1Y = os.path.join(CSV_DIR, 'titan-chart-1y.csv')
+PATH_TO_CSV_CHART_3Y = os.path.join(CSV_DIR, 'titan-chart-3y.csv')
+PATH_TO_CSV_CHART_5Y = os.path.join(CSV_DIR, 'titan-chart-5y.csv')
+PATH_TO_CSV_CHART_10Y = os.path.join(CSV_DIR, 'titan-chart-10y.csv')
+PATH_TO_CSV_CHART_ALL = os.path.join(CSV_DIR, 'titan-chart-all.csv')
+PATH_TO_CSV_STATS = os.path.join(CSV_DIR, 'titan-stats.csv')
 
 if DEBUG:
     CORS_ORIGIN_ALLOW_ALL = True
